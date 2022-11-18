aa1d2d03cbfba5a55d7c893ac572a6b4cecb4b97
Joey Tsui <joeytsui131@gmail.com>
2022-11-18 08:53:26 -0500

    "run3 23 mins"

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,7 +1,7 @@
 class Player
     def get_move
         puts "enter a position with coordinates separated with a space like '4 7'"
-        pos = gets.chomp.splits(" ").map(&:to_i)
+        pos = gets.chomp.split(" ").map(&:to_i)
         return pos
     end
 end
