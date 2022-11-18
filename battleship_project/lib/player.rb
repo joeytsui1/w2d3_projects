76cea06479f6c0c19d37566c53bc293d69ff209a
Joey Tsui <joeytsui131@gmail.com>
2022-11-18 08:47:29 -0500

    "player done"

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,7 @@
 class Player
-    
+    def get_move
+        puts "enter a position with coordinates separated with a space like '4 7'"
+        pos = gets.chomp.splits(" ").map(&:to_i)
+        return pos
+    end
 end
