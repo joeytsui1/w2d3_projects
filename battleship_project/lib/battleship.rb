7ff06f4c31429b9755f37fc9a96c7b55e01e158b
Joey Tsui <joeytsui131@gmail.com>
2022-11-20 11:04:46 -0500

    "battleship run 4 20 mins"

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -3,4 +3,48 @@ require_relative "player"
 
 class Battleship
 
+    def initialize (num)
+        @board = Board.new(num)
+        @player = Player.new
+        @remaining_misses = @board.size / 2
+    end
+
+    attr_reader :board, :player
+
+    def start_game
+        @board.place_random_ships
+
+        puts @board.num_ships
+
+        @board.print
+    end
+
+    def lose?
+        if @remaining_misses <= 0
+            puts "you lose"
+            return true
+        end
+        return false
+    end
+
+    def win?
+        if @board.num_ships == 0
+            puts "you win"
+            return true
+        end
+        return false
+    end
+
+    def game_over?
+        win? || lose?
+    end
+
+    def turn 
+        pos = @player.get_move
+        if !@board.attack(pos)
+            @remaining_misses -= 1
+        end
+        @board.print
+        puts @remaining_misses
+    end
 end 
--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -4,68 +4,72 @@ class Board
       puts row.join(" ")
     end
   end
-  attr_reader :size
-  def initialize (num)
-    @grid = Array.new(num) {Array.new(num, :N)}
-    @size = num * num
-  end
-
-  def [](pos)
-    row, col = pos
-    @grid[row][col]
-  end
-
-  def []=(pos,val)
-    row, col = pos
-    @grid[row][col] = val
-  end
-
-  def num_ships
-    @grid.flatten.count {|ele| ele == :S }
-  end
-
-  def attack (pos)
-    if self[pos] == :S 
-      self[pos] = :H
-      puts "you have sunken my battleship"
-      return true
-    else
-      self[pos] = :X 
-      return false
+    def initialize  (num)
+      @grid = Array.new(num) {Array.new(num, :N)}
+      @size = num * num 
     end
-  end
 
-  def place_random_ships
-    max_ships = @size * 0.25
-    while max_ships > self.num_ships
-      row = rand(0...@grid.length)
-      col = rand(0...@grid.length)
-      pos = [row, col]
-      self[pos] = :S
+    attr_reader :size
+
+    def [](pos)
+      row,col = pos
+      @grid[row][col]
     end
-  end
 
-  def hidden_ships_grid
-    arr = []
-    @grid.each do |row|
-      sub = []
-      row.each do |ele|
-        if ele == :S 
-          sub << :N 
-        else
-          sub << ele
-        end
+    def []= (pos, val)
+      row, col = pos
+      @grid[row][col] = val
+    end
+
+    def num_ships
+      @grid.flatten.count {|ele| ele == :S }
+    end
+
+    def attack (pos)
+      if self[pos] == :S 
+        self[pos] = :H 
+        puts "you have sunken my battleship!"
+        return true
+      else
+        self[pos] = :X 
+        return false
       end
-      arr << sub
     end
-    return arr
-  end
 
-  def cheat
-    Board::print_grid(@grid)
-  end
+    def place_random_ships
+      max_ship = @size * 0.25
 
-  def print
-    Board::print_grid(hidden_ships_grid)
-  end
+      while self.num_ships < max_ship
+        row = rand(0...@grid.length)
+        col = rand(0...@grid.length)
+        pos = [row, col]
+        self[pos] = :S
+      end
+    end
+
+    def hidden_ships_grid
+      arr = []
+
+      @grid.each do |row|
+        sub = []
+        row.each do |ele|
+          if ele == :S 
+            sub << :N 
+          else
+            sub << ele
+          end
+        end
+        arr << sub
+      end
+      return arr
+    end
+
+    def cheat
+      Board.print_grid(@grid)
+    end
+
+    def print
+      Board.print_grid(hidden_ships_grid)
+    end
+
 end
